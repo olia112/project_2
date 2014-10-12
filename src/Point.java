927a3206985f153d0f8d375ac7c7d4f1320ddb48
olia112 <geraskina.olia2010@yandex.ru>
2014-10-12 16:18:53 +0400

    Create Point.java

---
+++ src/Point.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Оля on 12.10.2014.
+ */
+public class Point {
+    double x;
+    double y;
+    public Point (double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+}
