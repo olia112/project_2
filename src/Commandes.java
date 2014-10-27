0e1a0577d944db0671435e160822ada014704356
olia112 <geraskina.olia2010@yandex.ru>
2014-10-27 16:17:43 +0300

    Update Commandes.java

--- src/Commandes.java
+++ src/Commandes.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 public class Commandes {
     public Address[] list = new Address[0];
-    public int[] t = new int[0];
+    //public int[] t = new int[0];
 
     public void Add(Address newaddr) {
         Address[] newlist = new Address[list.length + 1];
@@ -18,6 +18,7 @@ public class Commandes {
 
     public int[] find(String name) {
         int j = 0;
+        int[] t = new int[0];
         for (int i = 0; i < list.length; i++) {
             if (list[i].name.equals(name)) {
                 int[] w = new int[t.length + 1];
@@ -26,13 +27,13 @@ public class Commandes {
                 }
                 w[j] = i;
                 j++;
-                System.out.println("Индекс: " + i + ") Имя: " + list[i].name + " Телефон: " + list[i].phone + " Email: " + list[i].email);
+                //System.out.println("Индекс: " + i + ") Имя: " + list[i].name + " Телефон: " + list[i].phone + " Email: " + list[i].email);
                 t = w;
             }
         }
-        if (t.length == 0) {
-            System.out.println("Человек с таким именем в книге отсутствует");
-        }
+ //       if (t.length == 0) {
+ //         System.out.println("Человек с таким именем в книге отсутствует");
+ //       }
         return t;
     }
 
@@ -42,7 +43,6 @@ public class Commandes {
 
 
     public void delete(int index) {
-        if (index < list.length) {
             Address[] newlist = new Address[list.length - 1];
             int j = 0;
             for (int i = 0; i < list.length; i++) {
@@ -51,7 +51,6 @@ public class Commandes {
                 j++;
             }
             list = newlist;
-        } else System.out.println("Запись с таким индексом отсутствует");
     }
 
 
@@ -67,22 +66,12 @@ public class Commandes {
 
     }
 
-    public void izmenenie() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите индекс записи");
-        int l = in.nextInt();
-        if (l < list.length) {
-            System.out.println("Введите новое имя");
-            String n = in.next();
-            System.out.println("Введите новый телефон");
-            String p = in.next();
-            System.out.println("Введите новый email");
-            String e = in.next();
-            list[l].name = n;
-            list[l].phone = p;
-            list[l].email = e;
-        } else System.out.println("Запись с таким индексом отсутствует");
-    }
+    public void izmenenie(int l, String n1, String p1, String e1) {
+            list[l].name = n1;
+            list[l].phone = p1;
+            list[l].email = e1;
+    }    
     public void print (Address entry){
         System.out.println("Имя: " + entry.name + " Телефон: " + entry.phone + " Email: " + entry.email);
     }
+}
